52809903aad6bd290aea1831ad4caa6746718a07
NiLaScience <nils.lang@bli.uzh.ch>
2025-01-14 23:38:24 +0100

    first round of deployment fixes

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,18 @@
 import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
-  })
-}
-
 declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+  var prisma: PrismaClient | undefined
 }
 
-export const prisma = globalThis.prisma ?? prismaClientSingleton()
+let prisma: PrismaClient
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
+
+export { prisma }
